673f63e6ea91e6a1b654b13df0af610d470ef7f2
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-03 10:08:52 +0700

    Filament Dependant Select Options

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -3,20 +3,25 @@
 namespace App\Filament\Resources;
 
 use Filament\Forms;
+use App\Models\City;
 use Filament\Tables;
+use App\Models\State;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use App\Models\Employee;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
+use Illuminate\Support\Collection;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Section;
+
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\DatePicker;
 use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\EmployeeResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\EmployeeResource\RelationManagers;
-
 
 class EmployeeResource extends Resource
 {
@@ -31,9 +36,32 @@ class EmployeeResource extends Resource
         return $form
             ->schema([
                 Section::make('Relationship')->schema([
-                    Select::make('country_id')->relationship('country', 'name')->searchable()->preload()->required(),
-                    Select::make('state_id')->relationship('state', 'name')->searchable()->preload()->required(),
-                    Select::make('city_id')->relationship('city', 'name')->searchable()->preload()->required(),
+                    Select::make('country_id')
+                        ->relationship('country', 'name')
+                        ->searchable()
+                        ->preload()
+                        ->afterStateUpdated(function (Set $set) {
+                            $set('state_id', null);
+                            $set('city_id', null);
+                        })
+                        ->live(),
+                    Select::make('state_id')
+                        ->options(fn (Get $get): Collection => State::query()
+                            ->where('country_id', $get('country_id'))
+                            ->pluck('name', 'id'))
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->afterStateUpdated(fn (Set $set) => $set('city_id', null))
+                        ->required(),
+                    Select::make('city_id')
+                        ->options(fn (Get $get): Collection => City::query()
+                            ->where('state_id', $get('state_id'))
+                            ->pluck('name', 'id'))
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->required(),
                     Select::make('department_id')->relationship('department', 'name')->searchable()->preload()->required(),
                 ])->columns(2),
                 Section::make('User Name')->description('Put The user Name Detail')->schema([
