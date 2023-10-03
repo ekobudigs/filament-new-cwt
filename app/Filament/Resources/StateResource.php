9ff157bad24e75514066d8f84151d55c635ec64a
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-03 11:11:31 +0700

    update colum table state show and seacrh

--- app/Filament/Resources/StateResource.php
+++ app/Filament/Resources/StateResource.php
@@ -2,18 +2,19 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\StateResource\Pages;
-use App\Filament\Resources\StateResource\RelationManagers;
-use App\Models\State;
 use Filament\Forms;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\State;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\StateResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\StateResource\RelationManagers;
 
 class StateResource extends Resource
 {
@@ -42,8 +43,11 @@ class StateResource extends Resource
     {
         return $table
             ->columns([
-                //
-            ])
+                TextColumn::make('country.name')->sortable()->searchable(isIndividual: true, isGlobal: false),
+                TextColumn::make('name')->label('State Name')->sortable()->searchable(isIndividual: true)->visible(!auth()->user()->email == 'eko@gmail.com'),
+                TextColumn::make('created_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
+            ])->defaultSort('country.name', 'desc')
             ->filters([
                 //
             ])
