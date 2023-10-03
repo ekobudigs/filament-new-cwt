3297a36bcf20702727c07483f716d86bf0e1dce8
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-03 08:48:48 +0700

    Update Model CIty function state

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class City extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'state_id',
+        'name'
+    ];
+
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
 }
